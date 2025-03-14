de3926b20f1d116a67731d60fb2b95d147217ded
MZahidKamal <kamal.md.zahid@gmail.com>
2025-03-14 01:50:21 +0100

    TanstackQuery installed and configured properly.

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,10 @@ import AboutPage from "./Pages/AboutPage/AboutPage.jsx";
 import SignUpPage from "./Pages/SignUpPage/SignUpPage.jsx";
 import SignInPage from "./Pages/SignInPage/SignInPage.jsx";
 import AuthProvider from "./Providers/AuthProvider.jsx";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+
+
+const queryClient = new QueryClient()
 
 
 const root = document.getElementById("root");
@@ -17,14 +21,16 @@ ReactDOM.createRoot(root).render(
     <StrictMode>
         <BrowserRouter>
             <AuthProvider>
-                <Routes>
-                    <Route path={'/'} element={<MainLayout></MainLayout>}>
-                        <Route path={'/'} element={<HomePage></HomePage>}></Route>
-                        <Route path={'/about'} element={<AboutPage></AboutPage>}></Route>
-                        <Route path={'/sign-up'} element={<SignUpPage></SignUpPage>}></Route>
-                        <Route path={'/sign-in'} element={<SignInPage></SignInPage>}></Route>
-                    </Route>
-                </Routes>
+                <QueryClientProvider client={queryClient}>
+                    <Routes>
+                        <Route path={'/'} element={<MainLayout></MainLayout>}>
+                            <Route path={'/'} element={<HomePage></HomePage>}></Route>
+                            <Route path={'/about'} element={<AboutPage></AboutPage>}></Route>
+                            <Route path={'/sign-up'} element={<SignUpPage></SignUpPage>}></Route>
+                            <Route path={'/sign-in'} element={<SignInPage></SignInPage>}></Route>
+                        </Route>
+                    </Routes>
+                </QueryClientProvider>
             </AuthProvider>
         </BrowserRouter>
     </StrictMode>
