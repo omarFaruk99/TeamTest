5a1858a0675165cc13ab547e522776d3ea3a84bf
omar faruk <2035faruk@gmail.com>
2025-03-23 17:34:57 +0600

    Deleted all files

--- src/SharedUtilities/SharedUtilities.jsx
+++
@@ -1,5 +0,0 @@
-export const BASE_URL = import.meta.env.VITE_node_environment === 'production'
-    ? 'https://gadgetswap-rental-marketplace-server-side.vercel.app'
-    : 'http://localhost:3000';
-
-console.log(`Current BASE_URL: ${BASE_URL}`);
